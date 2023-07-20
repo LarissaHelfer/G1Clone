d685a2bdfbb40a85419f7e4cadccae96fa832a39
Larissa Helfer <joanahelfer@gmail.com>
2023-07-20 11:26:06 -0300

    third commit

--- scripts/script.js
+++ scripts/script.js
@@ -71,3 +71,60 @@ document.addEventListener("DOMContentLoaded", function () {
     elementSecond.querySelector(".box .right").innerHTML = resultTopRight;
 
 });
+
+
+function expandInput() {
+    document.getElementById("divBusca").classList.add("alteraCaixa");
+  }
+  
+  function resetInput() {
+    document.getElementById("divBusca").classList.remove("alteraCaixa");
+  }
+
+//funcção para abrir menu
+function abrirMenu() {
+    document.getElementById("openMenu").classList.toggle("show");
+  }
+  
+//quando for sair do menu
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  document.getElementById('botao').onclick = function(){
+    location.href = 'https://login.globo.com/login/6870/connect-confirm?url=https%3A%2F%2Fid.globo.com%2Fauth%2Frealms%2Fglobo.com%2Flogin-actions%2Fauthenticate%3Fsession_code%3D4M6g97Sjc9cmqy2CfTQB69uXvdXHI0Z-WKjOo3m9qes%26execution%3Db5dd88dc-447e-468f-945e-e7c7de4883b7%26client_id%3Dbarra%2540apps.globoid%26tab_id%3DOtOYlsoJSeQ%26request-context%3DqSUOZ7&error=&request-context=qSUOZ7'
+  }
+
+  function updateFilter() {
+    const procuraNoticia = document.getElementById('txtBusca').value;
+    localStorage.setItem('noticiaFiltra', procuraNoticia);
+    window.location.href = 'https://vscode.dev/github/LarissaHelfer/G1Clone/blob/main/scripts/notice.js'; // Redirect to filtered.html
+  }
+
+  function displayFilteredList(list) {
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+  
+    list.forEach(item => {
+      const listItem = document.createElement('li');
+      listItem.textContent = item.name;
+      resultado.appendChild(listItem);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const procuraNoticia = localStorage.getItem('noticia');
+    const filtraNoticia = notice.filter(item =>
+      item.name.toLowerCase().includes(procuraNoticia.toLowerCase())
+    );
+    displayFilteredList(filtraNoticia);
+  });
